f3ba88794a50911cf93cddf9d213b607cdb640df
Kelly Suitor <kellysuitor@Kellys-MacBook-Pro.local>
2019-10-26 13:43:06 -0400

    Finished hw-02 - Kelly

--- pset-iterators.js
+++ pset-iterators.js
@@ -43,6 +43,16 @@ console.log('Problem 1:')
 
 // Add your code below this line
 
+// Find and return the first instance of a President 
+// with political Whig Party political affiliation
+const firstWhigPresident = presidents.find(function(president) {
+  if (president.party === 'Whig') {
+    return president.president
+  }
+})
+
+console.log(firstWhigPresident.president)
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -65,6 +75,18 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 2:')
 
 // Add your code below this line
+
+// Filter through all the president objects in the array
+// If the first name of the president matches 'James' 
+// Return that respective president's object
+const presidentsNamedJames = presidents.filter(function(president) {
+  firstName = president.president.split(' ')[0]
+  if (firstName === 'James') {
+    return president.president
+  }
+})
+
+console.log(presidentsNamedJames)
 
 // Add your code above this line
 
@@ -93,6 +115,17 @@ console.log('Problem 3:')
 
 // Add your code below this line
 
+const presidentialParties = []
+
+// Filter the Presidents and extract their respective
+// Party to the presidentialParties array
+const cyclePresidents = presidents.filter(function(president) {
+    presidentialParties.push(president.party)
+})
+
+console.log(presidentialParties)
+
+
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -114,6 +147,17 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 4:')
 
 // Add your code below this line
+
+// Filter the presidents data and convert took office date data to int
+// Return the president's data if he took office between 1850 - 1900
+const presidentsBetween1850and1900 = presidents.filter(function(president) {
+  const tookOfficeInt = parseInt(president.took_office)
+  if (tookOfficeInt >= 1850 && tookOfficeInt <= 1900 ) {
+    return president
+  }
+})
+
+console.log(presidentsBetween1850and1900)
 
 // Add your code above this line
 
@@ -138,6 +182,15 @@ console.log('Problem 5:')
 
 // Add your code below this line
 
+// Filter through the presidents
+// If the death year is false/null then return the president's object
+const livingPresidents = presidents.filter(function(president) {
+  if (!president.death_year) {
+    return president
+  }
+})
+
+console.log(livingPresidents)
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -160,6 +213,15 @@ Expected Result Shape: A string representing the full name of the president that
 console.log('Problem 6:')
 
 // Add your code below this line
+// Find the first preisdent with Republican party identification
+// and return that president's object
+const firstRepublican = presidents.find(function(president) {
+  if (president.party === 'Republican') {
+    return president
+  }
+})
+
+console.log(firstRepublican)
 
 // Add your code above this line
 
@@ -183,6 +245,21 @@ Expected Result Shape: An array of objects (with each object representing a pres
 console.log('Problem 7:')
 
 // Add your code below this line
+
+const shortTermPresidents = presidents.filter(function(president) {
+  const tookOfficeInt = parseInt(president.took_office)
+  const leftOfficeInt = parseInt(president.left_office)
+  // Determine time in office in years
+  const timeInOffice = leftOfficeInt - tookOfficeInt
+
+  // Only return short term presidents - in office for less
+  // than 4 years
+  if (timeInOffice < 4 ) {
+    return president
+  }
+})
+
+console.log(shortTermPresidents)
 
 // Add your code above this line
 
